6f113734e4a5cb05dee518bcd50452af3965f63d
Paweł Ryszawa <pablo53@poczta.onet.eu>
2020-01-22 23:19:06 +0100

    Setting numbers on Saietk ProFlight Multi Panel.

--- hid-saitek-proflight.c
+++ hid-saitek-proflight.c
@@ -15,6 +15,7 @@
 
 #include <linux/proc_fs.h>
 #include <linux/uaccess.h>
+#include <linux/ctype.h>
 
 
 #define SWITCH(x) ((x) ? "ON " : "OFF")
@@ -39,6 +40,10 @@
                 ((x) == RADIOPANEL_MODE_XPDR) ? "XPDR" : \
                 "   ")
 
+#define PANEL_DIGIT_MINUS 0xe0
+#define PANEL_DIGIT_DOT   0xd0
+#define PANEL_DIGIT_NULL  0xff
+
 #ifndef USB_VENDOR_ID_SAITEK
 #  define USB_VENDOR_ID_SAITEK 0x06a3
 #endif
@@ -51,6 +56,7 @@
 #endif
 
 #define MAX_BUFFER 256
+#define SAITEK_HID_BUF_LEN 13
 
 #define RADIOPANEL_MODE_COM1 1
 #define RADIOPANEL_MODE_COM2 2
@@ -67,6 +73,7 @@
 #define MULTIPANEL_MODE_CRS 5
 
 struct proflight_radiopanel {
+        struct proflight *parent;
         unsigned int act_stby1 : 1;
         unsigned int act_stby2 : 1;
         int mode1;
@@ -76,6 +83,7 @@ struct proflight_radiopanel {
 };
 
 struct proflight_multipanel {
+        struct proflight *parent;
         unsigned int hdg : 1;
         unsigned int nav : 1;
         unsigned int ias : 1;
@@ -95,6 +103,8 @@ struct proflight_multipanel {
         int flaps; // accumulated movement (DN - decreases, UP - increases)
         int pitch_trim; // accumulated movement (DN - decreases, UP - increases)
         int knob; // accumulated movement (left - decreases, right - increases)
+        char display0[5];
+        char display1[5];
 };
 
 union proflight_panel_data
@@ -109,8 +119,33 @@ struct proflight {
         const char *proc_name;
         struct proc_dir_entry *proflight_dir_entry;
         union proflight_panel_data data;
+        struct hid_device *hdev;
+        __u8 *dmabuf;
         char buffer[MAX_BUFFER];
 };
+
+static int saitek_parse_multipanel_display(char *display, const char *msg, size_t size)
+{
+        int dig = 0;
+        int i = 0;
+        char ch;
+
+        while (dig < 5 && i < size) {
+                ch = msg[i++];
+                if (ch == ' ')
+                        display[dig++] = PANEL_DIGIT_NULL;
+                else if (ch == '-')
+                        display[dig++] = PANEL_DIGIT_MINUS;
+                else if (ch == '.')
+                        display[dig++] = PANEL_DIGIT_DOT;
+                else if (isdigit(ch))
+                        display[dig++] = ch - '0';
+                else
+                        display[dig++] = PANEL_DIGIT_NULL;
+        }
+
+        return i;
+}
 
 static ssize_t saitek_proc_read_radiopanel(struct proflight_radiopanel *radiopanel,
                 struct file *f, char __user *buf, size_t count, loff_t *offset)
@@ -168,23 +203,64 @@ static ssize_t saitek_proc_read(struct file *f, char __user *buf, size_t count, 
         return -ENXIO; // default
 }
 
-static ssize_t saitek_proc_write(struct file *f, const char __user *buf, size_t count, loff_t *offset)
+static ssize_t saitek_proc_write_radiopanel(struct proflight_radiopanel *radiopanel,
+                struct file *f, const char __user *buf, size_t count, loff_t *offset)
 {
-        struct proflight *data;
-
-        if (*offset >= MAX_BUFFER)
-                return EFBIG;
-        data = PDE_DATA(file_inode(f));
-        if (!data) {
-                printk(KERN_ERR "Cannot find Saitek ProFlight device data.\n");
-                return -EIO;
-        }
         if (*offset + count >= MAX_BUFFER)
                 count = MAX_BUFFER - *offset;
-        copy_from_user(data->buffer, buf, count);
+        copy_from_user(&(radiopanel->parent->buffer[*offset]), buf, count);
         *offset += count;
 
         return count;
+}
+
+static ssize_t saitek_proc_write_multipanel(struct proflight_multipanel *multipanel,
+                struct file *f, const char __user *buf, size_t count, loff_t *offset)
+{
+        int res;
+
+        if (*offset + count >= MAX_BUFFER)
+                count = MAX_BUFFER - *offset;
+        copy_from_user(&(multipanel->parent->buffer[*offset]), buf, count);
+        *offset += count;
+
+        multipanel->parent->dmabuf[0] = 0; // also: report id
+        saitek_parse_multipanel_display(&(multipanel->parent->dmabuf[1]), &(multipanel->parent->buffer[0]), 5);
+        saitek_parse_multipanel_display(&(multipanel->parent->dmabuf[6]), &(multipanel->parent->buffer[6]), 5);
+        multipanel->parent->dmabuf[11] = 0; // TODO
+        multipanel->parent->dmabuf[12] = 0;
+
+        res = hid_hw_raw_request(multipanel->parent->hdev,
+                        multipanel->parent->dmabuf[0], multipanel->parent->dmabuf,
+                        13, HID_FEATURE_REPORT, HID_REQ_SET_REPORT);
+        if (res < 0)
+                printk(KERN_ERR "Error sending report: %d.\n", res);
+
+        return count;
+}
+
+static ssize_t saitek_proc_write(struct file *f, const char __user *buf, size_t count, loff_t *offset)
+{
+        struct proflight *driver_data;
+
+        if (*offset >= MAX_BUFFER)
+                return EFBIG;
+        driver_data = PDE_DATA(file_inode(f));
+        if (!driver_data) {
+                printk(KERN_ERR "Cannot find Saitek ProFlight device data.\n");
+                return -EIO;
+        }
+        
+        switch(driver_data->product_id) {
+        case USB_DEVICE_ID_SAITEK_PROFLIGHT_RADIOPANEL:
+                return saitek_proc_write_radiopanel(driver_data->data.radiopanel,
+                                f, buf, count, offset);
+        case USB_DEVICE_ID_SAITEK_PROFLIGHT_MULTIPANEL:
+                return saitek_proc_write_multipanel(driver_data->data.multipanel,
+                                f, buf, count, offset);
+        }
+
+        return -ENXIO;
 }
 
 static const struct file_operations saitek_proc_fops = {
@@ -231,11 +307,25 @@ static int saitek_proflight_alloc_panel_data(
                 hid_err(hdev, "No memory for Saitek ProFlight driver data.\n");
                 return -ENOMEM;
         }
+        switch (id->product) {
+        case USB_DEVICE_ID_SAITEK_PROFLIGHT_RADIOPANEL:
+                panel_data.radiopanel->parent = *driver_data_p;
+                break;
+        case USB_DEVICE_ID_SAITEK_PROFLIGHT_MULTIPANEL:
+                panel_data.multipanel->parent = *driver_data_p;
+                break;
+        }
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
         (*driver_data_p)->data = panel_data;
 #pragma GCC diagnostic pop
+
+        (*driver_data_p)->dmabuf = devm_kzalloc(&hdev->dev, SAITEK_HID_BUF_LEN, GFP_KERNEL);
+        if (!((*driver_data_p)->dmabuf)) {
+                hid_err(hdev, "No memory for Saitek ProFlight HID buffer.\n");
+                return -ENOMEM;
+        }
 
         return 0;
 }
@@ -274,6 +364,7 @@ static int saitek_proflight_probe(struct hid_device *hdev, const struct hid_devi
         hid_set_drvdata(hdev, driver_data);
         driver_data->product_id = id->product;
         driver_data->proc_name = proc_name(id->product);
+        driver_data->hdev = hdev;
 
         driver_data->proflight_dir_entry = proc_create_data(driver_data->proc_name,
                         0666, NULL, &saitek_proc_fops, driver_data);
